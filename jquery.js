b687590f0de72c86e74a1798bc30c46fdac5c655
Amee Srivastava <52029498+amee6394@users.noreply.github.com>
2020-12-10 10:34:26 +0530

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,39 +1,57 @@
+        $(document).ready(function () {
+            var x = 0;
 
-$(document).ready(function () {
-    var x = 0;
-    $('.btn-next').click(function () {
-
-        x = (x <= 300) ? (x + 100) : 0;
-        $('figure').css('left', -x + '%');
-    });
-    $('.btn-prev').click(function () {
-
-        x = (x >= 100) ? (x - 100) : 400;
-        $('figure').css('left', -x + '%');
-    });
+            //**********Automatic silder*************** */
+            setInterval(function(){
+                x = (x <= 300) ? (x + 100) : 0;
+                $('figure').css('left', -x + '%');
+            },4000);
 
 
+        //**************next button***************** */
+            $('.btn-next').click(function () {
 
-    $(".dot").on("click", function () {
+                x = (x <= 300) ? (x + 100) : 0;
+                $('figure').css('left', -x + '%');
+            });
+        //******************prev button***********************/
+            $('.btn-prev').click(function () {
 
-        x = 0;
-        $('figure').css('left', -x + '%');
-    });
+                x = (x >= 100) ? (x - 100) : 400;
+                $('figure').css('left', -x + '%');
+            });
 
-    $(".dot2").on("click", function () {
-        x = 100;
-        $('figure').css('left', -x + '%');
-    });
-    $(".dot3").on("click", function () {
-        x = 200;
-        $('figure').css('left', -x + '%');
-    });
-    $(".dot4").on("click", function () {
-        x = 300;
-        $('figure').css('left', -x + '%');
-    });
-    $(".dot5").on("click", function () {
-        x = 400;
-        $('figure').css('left', -x + '%');
-    });
-});
+
+        //******************first dot*****************************/
+            $(".dot").on("click", function () {
+
+                x = 0;
+                $('figure').css('left', -x + '%');
+            });
+        //******************second dot*****************************/
+
+            $(".dot2").on("click", function () {
+                x = 100;
+                $('figure').css('left', -x + '%');
+            });
+        //******************third dot**************************** */
+
+            $(".dot3").on("click", function () {
+                x = 200;
+                $('figure').css('left', -x + '%');
+            });
+
+        //******************fourth dot**************************** */
+
+            $(".dot4").on("click", function () {
+                x = 300;
+                $('figure').css('left', -x + '%');
+            });
+
+        //******************fifth dot**************************** */
+
+            $(".dot5").on("click", function () {
+                x = 400;
+                $('figure').css('left', -x + '%');
+            });
+        });
